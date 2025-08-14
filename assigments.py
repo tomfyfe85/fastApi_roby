133148b5d7da7aeac2e19e66b1285958f330c866
tomfyfe <tomfyfe85@gmail.com>
2025-08-14 14:24:07 +0100

    if else elif

--- assigments.py
+++ assigments.py
@@ -1,7 +1,23 @@
-# Lists
+"""
+Lists
+"""
+# zoo = [1,2,3,4,5]
+# zoo.pop(3)
+# zoo.append(6)
+# zoo.pop(0)
+# print(zoo)
+# print(zoo[0:3])
 
-zoo = [1,2,3,4,5]
-zoo.append(6)
-zoo.pop(0)
-print(zoo)
-print(zoo[0:3])
+
+# IF ELSE ELIF
+
+GRADE = 80
+
+if GRADE >= 90 and GRADE <= 100:
+    print("A")
+elif GRADE >= 80 and GRADE <= 89:
+    print("B")
+elif GRADE >= 70 and GRADE <= 79:
+    print("C")
+else:
+    print('F')
