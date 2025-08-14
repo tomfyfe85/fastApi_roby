76495ffe00e8ffbbccbedddf7849c0a4596b0ee1
tomfyfe <tomfyfe85@gmail.com>
2025-08-14 15:08:26 +0100

    loops assignment

--- assigments.py
+++ assigments.py
@@ -11,13 +11,34 @@ Lists
 
 # IF ELSE ELIF
 
-GRADE = 80
+# GRADE = 80
 
-if GRADE >= 90 and GRADE <= 100:
-    print("A")
-elif GRADE >= 80 and GRADE <= 89:
-    print("B")
-elif GRADE >= 70 and GRADE <= 79:
-    print("C")
-else:
-    print('F')
+# if GRADE >= 90 and GRADE <= 100:
+#     print("A")
+# elif GRADE >= 80 and GRADE <= 89:
+#     print("B")
+# elif GRADE >= 70 and GRADE <= 79:
+#     print("C")
+# else:
+#      print('F')
+
+
+#LOOPS
+my_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+# i = 0
+# while i < len(my_list):
+#     for x in my_list:
+#         if x == "Monday":
+#             i += 1
+#             continue
+#         else:
+#             print(x * 3)
+#             i += 1
+
+x = 0
+while x < 3:
+    x += 1
+    for i in my_list:
+
+        print(i)
